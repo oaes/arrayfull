88824dc6e5fe77ec85334bfeb609faee658e0c86
Oaes Kuruni <oaeskuruni27@gmail.com>
2019-07-25 19:02:05 +0600

    added a file

---
+++ main.js
@@ -0,0 +1,25 @@
+//********** */array in js**************
+
+
+var arr = [2, 3, 4, 5, 1]
+
+console.log(arr[3]);
+
+//for checking length of array
+console.log(arr.length);
+
+arr.push(9)
+//for adding new array
+console.log(arr);
+
+arr.pop()
+//for removing last array
+console.log(arr);
+
+//for serial small to big
+arr.sort()
+console.log(arr);
+
+//for serial big to  small
+arr.reverse()
+console.log(arr);
